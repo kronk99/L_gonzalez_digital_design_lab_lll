2ed43b435da3715fa20e6d3ed6fbcac55912b89c
Andguzman <105981706+Andguzman@users.noreply.github.com>
2024-04-18 23:57:04 -0600

    FSM
    Correcciones y comentarios finales a la FSM

--- FSM.sv
+++ FSM.sv
@@ -1,33 +1,64 @@
-module FSM(input Btn, rst, clk,T,V, VJ, VP,NBarcos, output Timer, Turno, output logic CJug[4:0], CPC[4:0], Barcos[2:0], Casilla[4:0]);
+module FSM(input Btn, rst, clk,T,V, VJ, VP,RBarcos, input logic [2:0] NBarcos, output Timer, Turno, TurnoPC, SBarcos, CBarcos, CVictoria, CDerrota);
 
-logic [2:0] state, next_state; //Necesito 3 bits porque son 6 estados
+/*
+
+ENTRADAS
+
+Btn es el boton de la FPGA
+rst es el reset
+clk clock
+T es que han pasado 15 segundos
+V es que la casilla a jugar es valida (disparo)
+VJ es que el jugador ha ganado
+VP es que la maquina ha ganado
+NBarcos es la cantidad de barcos con los que jugar
+RBarcos es que ya se han terminado de colocar los NBarcos
+
+
+SALIDAS
+
+Timer es la senal para que el timer cuente
+Turno es 1 para jugador
+TurnoPC es 1 para PC
+SBarcos es 1 cuando estoy eligiendo la cantidad de barcos (enable del registro)
+CBarcos es 1 cuando se estan colocando barcos (enable del registro)
+CVictoria es 1 cuando se quiere comprobar victoria del jugador
+CDerrota es 1 cuando se quiere comprobar derrota del jugador 
+
+
+*/
+
+logic [3:0] state, next_state; //Necesito 3 bits porque son 6 estados
 
 //Reset
 always_ff @ (posedge clk or posedge rst)
-	if (rst) state = 2'b00;
+	if (rst) state = 4'b0000;
 	else
 		state = next_state;
 
 //Logica del siguiente estado
 always_comb
 	case(state)
-		3'b000: if (Btn) next_state = 3'b001; else next_state = 3'b000; //Estado de seleccion de # barcos
-		3'b001: if (Btn) next_state = 3'b010; else if (T) next_state = 3'b100; else next_state = 3'b001; //Estado de turno de jugador
-		3'b010: if (V) next_state = 3'b011; else next_state = 3'b001; //Estado comprobar casilla jugador
-		3'b011: if (VJ) next_state=3'b111; else next_state = 3'b100; //Estado comprobar victoria jugador
-		3'b100: next_state = 3'b101;//Estado turno PC
-		3'b101: if (V) next_state = 3'b110; else next_state = 3'b100;//Estado comprobar casilla PC
-		3'b110: if (VP) next_state = 3'b111; else next_state = 3'b001;//Estado comprobar victoria PC
-		3'b111: next_state = 3'b111;//Fin del juego
-		default: next_state = 3'b000;
+		4'b0000: if (Btn) next_state = 4'b1000; else next_state = 4'b0000; //Estado de seleccion de # barcos
+		4'b1000: if (RBarcos) next_state = 4'b0001; else next_state = 4'b0001; //Estado de colocar los barcos del jugador
+		4'b0001: if (Btn) next_state = 4'b0010; else if (T) next_state = 4'b0100; else next_state = 4'b0001; //Estado de turno de jugador
+		4'b0010: if (V) next_state = 4'b0011; else next_state = 4'b0001; //Estado comprobar casilla jugador
+		4'b0011: if (VJ) next_state=4'b0111; else next_state = 4'b0100; //Estado comprobar victoria jugador
+		4'b0100: next_state = 4'b0101;//Estado turno PC
+		4'b0101: if (V) next_state = 4'b0110; else next_state = 4'b0100;//Estado comprobar casilla PC
+		4'b0110: if (VP) next_state = 4'b0111; else next_state = 4'b0001;//Estado comprobar victoria PC
+		4'b0111: next_state = 4'b0111;//Fin del juego
+		default: next_state = 4'b0000;
 	endcase
 
 //output logic
 
-assign Timer = (state == 3'b001);// El timer se hace 1 para indicar que se debe iniciar la cuenta de 15 segundos para el turno del jugador
-assign Turno = (state == 3'b001); // Es 1 cuando es el turno del jugador, es 0 cuando es el turno de la PC
-assign cont = (state == 2'b01); //este es para decirle al registro que cuente
+assign Timer = (state == 4'b0001);// El timer se hace 1 para indicar que se debe iniciar la cuenta de 15 segundos para el turno del jugador
+assign Turno = (state == 4'b0001); // Es 1 cuando es el turno del jugador
+assign TurnoPC = (state == 4'b0100); //Turno de la PC
+assign SBarcos = (state == 4'b0000); 
+assign CBarcos = (state == 4'b1000); 
+assign CVictoria = (state == 4'b0011); 
+assign CDerrota = (state == 4'b0110); 
 
-assign sel_state = (state == 2'b11);//supongo que es el estado de error
-//debo hacer uno para el mux, antes estaba en b11 , que es el estado 3
 endmodule
